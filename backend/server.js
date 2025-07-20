111c9b986011417a3c5f7f82119ee92c36cb2052
rexwithluv <aaronaax@gmail.com>
2025-07-20 14:49:15 +0200

    feat: connect expressjs server and mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,56 +1,54 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
 
-const app = express();
-const PORT = 3000;
+const app = express()
+const PORT = 3000
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-const mongoUri = "mongodb://root:root@mongodb:27017/admin";
+const mongoUri = 'mongodb://root:root@localhost:27017/admin'
 
 try {
-  await mongoose.connect(mongoUri);
-  console.log("Express conectado a MongoDB con éxito!");
+  await mongoose.connect(mongoUri)
+  console.log('Express conectado a MongoDB con éxito!')
 } catch (e) {
-  console.log("Express no logró conectarse con MongoDB");
+  console.log(`Express no logró conectarse con MongoDB: ${e.message}`)
 }
 
 const quoteSchema = new mongoose.Schema({
   author: { type: String, required: true },
   quote: { type: String, required: true },
   note: { type: String, required: false },
-});
-const Quote = mongoose.model("Quote", quoteSchema);
+})
+const Quote = mongoose.model('Quotes', quoteSchema)
 
-app.get("/quotes", async (req, res) => {
+app.get('/quotes', async (req, res) => {
   try {
-    const quotes = await Quote.find();
-    res.status(200).json(quotes);
+    const quotes = await Quote.find()
+    res.status(200).json(quotes)
   } catch (e) {
-    res.status(500).json({ message: e.message });
+    res.status(500).json({ message: e.message })
   }
-});
+})
 
-app.post("/quotes", async (req, res) => {
-  const { author, quote, note } = req.body;
+app.post('/quotes', async (req, res) => {
+  const { author, quote, note } = req.body
 
   if (!author || !quote) {
-    return res
-      .status(400)
-      .json({ message: "El texto de la frase es obligatorio." });
+    return res.status(400).json({ message: 'El texto de la frase es obligatorio.' })
   }
 
-  const newQuote = new Quote({ author, quote, note });
+  const newQuote = new Quote({ author, quote, note })
   try {
-    const savedQuote = await newQuote.save();
-    res.status(201).json(savedQuote);
+    const savedQuote = await newQuote.save()
+    res.status(201).json(savedQuote)
   } catch (e) {
-    res.status(400).json({ message: e.message });
+    res.status(400).json({ message: e.message })
   }
-});
+})
 
 app.listen(PORT, () => {
-  console.log(`Servidor API escuchando en el puerto ${PORT}`);
-});
+  console.log(`Servidor API escuchando en el puerto ${PORT}`)
+})
