0362bc86cad336c6a4e815f79dcd5bfd0c7dfc71
rexwithluv <aaronaax@gmail.com>
2025-07-20 15:54:01 +0200

    feat(frontend): separate panel in new component and add key event listener

---
+++ frontend/src/components/QuotePanel.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref, type Ref } from 'vue';
+
+const getFormatedDate = (): string => {
+    return new Date().toLocaleString("es-ES", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    })
+}
+
+const formatedDate: Ref<string> = ref(getFormatedDate())
+const interval = ref(0)
+
+onMounted(() => {
+    interval.value = setInterval(() => {
+        formatedDate.value = getFormatedDate()
+    }, 1000)
+})
+
+onUnmounted(() => {
+    clearInterval(interval.value)
+})
+</script>
+
+<template>
+    <div class="page-wrapper">
+        <div class="content-box">
+            <p>{{ formatedDate }}</p>
+            <p>El contenido se adapta y el fondo gris es leve.</p>
+            <p>Puedes añadir más frases aquí para ver cómo crece.</p>
+        </div>
+    </div>
+</template>
--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,64 +1,40 @@
 <script setup lang="ts">
-import { ref, type Ref } from 'vue';
+import QuotePanel from '@/components/QuotePanel.vue';
+import { onMounted, onUnmounted } from 'vue';
 
-const getFormatedDate = (): string => {
-    return new Date().toLocaleString("es-ES", {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit"
-    })
+const handleKeyPress = (event: KeyboardEvent) => {
+    event.preventDefault();
+
+    switch (event.code) {
+        case "KeyI":
+            console.log("Create new quote (press I)")
+            break;
+
+        case "KeyH":
+            console.log("Display help (press H)")
+            break;
+
+        case "KeyC":
+            console.log("Changing quote (press C)")
+            break;
+
+        default:
+            console.log("No sé que tecla has tocado.")
+            break;
+    }
 }
 
-const formatedDate: Ref<string> = ref(getFormatedDate())
+onMounted(() => {
+    window.addEventListener("keydown", handleKeyPress)
+})
 
-setInterval(() => {
-    formatedDate.value = getFormatedDate()
-}, 1000)
+onUnmounted(() => {
+    window.removeEventListener("keydown", handleKeyPress)
+})
 </script>
 
 <template>
-    <div class="page-wrapper">
-        <div class="content-box">
-            <p>{{ formatedDate }}</p>
-            <p>El contenido se adapta y el fondo gris es leve.</p>
-            <p>Puedes añadir más frases aquí para ver cómo crece.</p>
-        </div>
-    </div>
+    <QuotePanel></QuotePanel>
 </template>
 
-<style scoped>
-.page-wrapper {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
-    background-color: #f0f0f0;
-    padding: 20px;
-    box-sizing: border-box;
-}
-
-.content-box {
-    background-color: #f7f7f7;
-    padding: 30px;
-    border-radius: 8px;
-    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.05);
-    max-width: 600px;
-    text-align: center;
-    margin: auto;
-}
-
-
-.content-box p {
-    margin-bottom: 10px;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    color: #333;
-    line-height: 1.6;
-}
-
-.content-box p:last-child {
-    margin-bottom: 0;
-}
-</style>
+<style scoped></style>
