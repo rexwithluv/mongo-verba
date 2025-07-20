079359c9d5f321ce2f116ebdecfcba68f74ff603
rexwithluv <aaronaax@gmail.com>
2025-07-20 14:34:29 +0200

    feat: add backend and frontend directories

---
+++ backend/server.js
@@ -0,0 +1,56 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+const mongoUri = "mongodb://root:root@mongodb:27017/admin";
+
+try {
+  await mongoose.connect(mongoUri);
+  console.log("Express conectado a MongoDB con éxito!");
+} catch (e) {
+  console.log("Express no logró conectarse con MongoDB");
+}
+
+const quoteSchema = new mongoose.Schema({
+  author: { type: String, required: true },
+  quote: { type: String, required: true },
+  note: { type: String, required: false },
+});
+const Quote = mongoose.model("Quote", quoteSchema);
+
+app.get("/quotes", async (req, res) => {
+  try {
+    const quotes = await Quote.find();
+    res.status(200).json(quotes);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
+
+app.post("/quotes", async (req, res) => {
+  const { author, quote, note } = req.body;
+
+  if (!author || !quote) {
+    return res
+      .status(400)
+      .json({ message: "El texto de la frase es obligatorio." });
+  }
+
+  const newQuote = new Quote({ author, quote, note });
+  try {
+    const savedQuote = await newQuote.save();
+    res.status(201).json(savedQuote);
+  } catch (e) {
+    res.status(400).json({ message: e.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor API escuchando en el puerto ${PORT}`);
+});
