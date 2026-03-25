cda2815423fc2ff89ddb2428959fcba0b494bca3
rexwithluv <aaronaax@gmail.com>
2026-03-25 22:39:56 +0100

    feat(scheduler): add cronjob.sh script to crontab

---
+++ scheduler/cronjob.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCHEDULER_PATH="$1"
+NODE_BIN=".nvm/versions/node/v24.14.1/bin/node"
+
+DELAY=$((RANDOM % (13 * 60 * 60)))
+DELAY_MINUTES=$((DELAY / 60))
+
+echo "Run scheduler.js (mongo-verba) with '$DELAY_MINUTES' minutes delay | '$SCHEDULER_PATH' scheduler path"
+
+sleep $DELAY && "$SCHEDULER_PATH"
--- scheduler/scheduler.js
+++ scheduler/scheduler.js
@@ -1,22 +1,22 @@
 // CRONTAB
-// 0 9 * * * sleep $((RANDOM \% (13 * 60 * 60))) && scheduler.js
+// 0 9 * * * /bin/bash $HOME/cronjob.sh "$HOME/scheduler.js" >> "$HOME/scheduler.log" 2>&1
 
 async function fetchQuote() {
   const response = await fetch('https://rexwithluv.dev/api/mongo-verba/quotes/random')
   const data = await response.json()
 
-  console.log(`Get Quote: ${data}`)
+  console.log('Get Quote: ', data)
 
   return data
 }
 
 async function sendNtfy(quoteObject) {
   const quote = quoteObject['quote']
-  const author = quoteObject['author'] || 'Anónimo'
+  const author = quoteObject['author'] || ''
 
   await fetch('https://ntfy.sh/rwl', {
     method: 'POST',
-    body: quote,
+    body: Buffer.from(quote, 'utf-8'),
     headers: {
       Title: author,
       Priority: 'high',
@@ -24,7 +24,7 @@ async function sendNtfy(quoteObject) {
     },
   })
 
-  console.log(`Sending ntfy: ${quoteObject}`)
+  console.log('Sending ntfy: ', quoteObject)
 }
 
 async function main() {
