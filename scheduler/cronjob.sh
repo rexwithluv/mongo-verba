dc110fb4e4306cec6deca6705ec637214b90771c
rexwithluv <aaronaax@gmail.com>
2026-03-31 21:41:53 +0200

    fix(scheduler): use $NODE_BIN variable instead node

--- scheduler/cronjob.sh
+++ scheduler/cronjob.sh
@@ -8,4 +8,4 @@ DELAY_MINUTES=$((DELAY / 60))
 
 echo "Run scheduler.js (mongo-verba) with '$DELAY_MINUTES' minutes delay | '$SCHEDULER_PATH' scheduler path"
 
-sleep $DELAY && node "$SCHEDULER_PATH"
+sleep $DELAY && $NODE_BIN "$SCHEDULER_PATH"
